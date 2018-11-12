f4d13b422695bd1b5157370ed463b5cf9b647be9
Nick Elliott <nicholasmelliott@yahoo.com>
2018-11-11 20:16:31 -0500

    Refactored Components in courses into a single CourseContainer component which receives course data through props.

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavLink, Route, Redirect } from 'react-router-dom';
 
-import HTML from './courses/HTML';
-import CSS from './courses/CSS';
-import JavaScript from './courses/JavaScript';
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
+import NotFound from './NotFound';
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -17,9 +17,10 @@ const Courses = ({match}) => (
     </div>
     
     <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} />} />
-    <Route path={`${match.path}/html`} component={HTML} />
-    <Route path={`${match.path}/css`} component={CSS} />
-    <Route path={`${match.path}/javascript`} component={JavaScript} />
+    <Route path={`${match.path}/html`} render={ () => <CourseContainer data={HTMLCourses} /> } />
+    <Route path={`${match.path}/css`} render={ () => <CourseContainer data={CSSCourses} /> } />
+    <Route path={`${match.path}/javascript`} render={ () => <CourseContainer data={JSCourses} /> } />
+    <Route component={NotFound} />
   </div>
 );
 
